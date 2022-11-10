d26b073efcd36008c37242c677dedf39961d3aea
Ashmyrasheed <ashmytr21@gmail.com>
2022-11-10 10:51:09 +0530

    first

--- admin.py
+++ admin.py
@@ -1,7 +1,11 @@
 import mysql.connector
-
-mydb = mysql.connector.connect(host = 'localhost' , user = 'root' , password = '' , database = 'admindb')
-mycursor = mydb.cursor()
+import sys
+try:
+    mydb = mysql.connector.connect(host = 'localhost' , user = 'root' , password = '' , database = 'admindb')
+    mycursor = mydb.cursor()
+except mysql.connector.Error as e:
+    sys.exit("data error")
+mycursor=mydb.cursor()
 
 
 while True:
@@ -30,7 +34,20 @@ while True:
 
     if(choice==1):
 
-        print('consumer enter selected')
+        print('consumer add selected')
+        code=input("enter the consumer code")
+        name=input("enter the name")
+        address=input("enter the address")
+        phno=input("enter the ph no")
+        email=input("enter the email")
+        try:
+            sql="INSERT INTO `consumer`(`code`, `name`, `address`, `phno`, `email`) VALUES(%s,%s,%s,%s,%s)"
+            data=(code,name,address,phno,email)
+            mycursor.execute(sql,data)
+            mydb.commit()
+            print("values insertred successfully")
+        except mysql.connector.Error as e:
+            sys.exit("view data error")
         
     elif(choice==2):
 
